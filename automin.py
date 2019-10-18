79394757581eb53052e41bf8dd51ecd42d73b0ac
werlang <pswerlang@gmail.com>
2019-10-18 05:13:48 -0300

    added files

---
+++ automin.py
@@ -0,0 +1,202 @@
+from __future__ import print_function
+import httplib, urllib, sys, json, re, os, requests
+
+#check if dev or prod mode
+mode = sys.argv[1]
+
+#load config from file
+#f = open('closure_config.json', 'r')
+f = open('closure_test.json', 'r')
+config = json.loads(f.read())
+f.close()
+
+total_files = len(config['files'])
+files_done = 0
+dev_path = {
+    "js": config['path']['js']['dev'],
+    "css": config['path']['css']['dev']
+}
+
+min_path = {
+    "js": config['path']['js']['min'],
+    "css": config['path']['css']['min']
+}
+
+html_path = config['path']['root']
+for h in config['files']:
+    files = {
+        "js": [],
+        "css": []
+    }
+    html_name = h['name']
+
+    #read php file
+    f = open("{}/{}".format(html_path, html_name), 'r')
+    file_php = f.read()
+    f.close()
+
+    changes = {
+        "js": False,
+        "css": False
+    }
+    for k in ['css', 'js']:
+
+        if mode == 'prod':
+            #insert prod script/link tag if does not exist
+            if k == 'js':
+                pattern = r'<script src=\"{}\/{}\.min\.js\"><\/script>'.format(min_path[k], html_name.split('.')[-2])
+            else:
+                pattern = r'<link rel=\'stylesheet\' href=\"{}\/{}\.min\.css\"\/>'.format(min_path[k], html_name.split('.')[-2])
+            
+            if not re.search(pattern, file_php):
+                #look for first dev file to know where to append prod
+                name = h[k]['files'][0]['name']
+
+                if k == 'js':
+                    pattern = r'<script[\w\s\=\-\/\"\']*?src=[\'\"]{}\/{}[\'\"][\w\s\=\-\/\"\'>]+?<\/script>'.format(dev_path[k], name)
+                    replace = "<script src=\"{}/{}.min.js\"></script>\n\t<script src=\"{}/{}\"></script>".format(min_path[k], html_name.split('.')[-2], dev_path[k], name)
+                else:
+                    pattern = r'<link[\w\s\=\-\/\"\']*?href=[\'\"]{}\/{}[\'\"]\/>'.format(dev_path[k], name)
+                    replace = "<link rel='stylesheet' href=\"{}/{}.min.css\"/>\n\t<link rel='stylesheet' href=\"{}/{}\"/>".format(min_path[k], html_name.split('.')[-2], dev_path[k], name)
+
+                file_php = re.sub(pattern, replace, file_php)
+            
+            #cut permission to dev dir
+            os.system("chmod 000 {}/{}".format(html_path, dev_path[k]))
+
+        for j in h[k]['files']:
+            name = j['name']
+
+            if mode == 'prod':     
+                #check if there id changes in dev file
+                dev_time = os.path.getmtime('{}/{}/{}'.format(html_path, dev_path[k], name))
+                if os.path.isfile('{}/{}/{}.min.{}'.format(html_path, min_path[k], html_name.split('.')[-2], k)):
+                    prod_time = os.path.getmtime('{}/{}/{}.min.{}'.format(html_path, min_path[k], html_name.split('.')[-2], k))
+                    if dev_time > prod_time:
+                        changes[k] = True
+                else:
+                    changes[k] = True
+
+                #store js/css files to compile later
+                f = open("{}/{}/{}".format(html_path, dev_path[k], name), 'r')
+                files[k].append(f.read())
+                f.close()
+
+                #erase prod script/link tags in wrong place
+                if k == 'js':
+                    pattern = r'<script[\w\s\=\-\/\"\']*?src=[\'\"]{}\/{}[\'\"][\w\s\=\-\/\"\'>]+?<\/script>'.format(min_path[k], name)
+                else:
+                    pattern = r'<link[\w\s\=\-\/\"\']*?href=[\'\"]{}\/{}[\'\"]\/>'.format(min_path[k], name)
+
+                file_php = re.sub(pattern, '', file_php)
+
+                #erase dev script/link tags
+                if k == 'js':
+                    pattern = r'<script[\w\s\=\-\/\"\']*?src=[\'\"]{}\/{}[\'\"][\w\s\=\-\/\"\'>]+?<\/script>[\s]*'.format(dev_path[k], name)
+                else:
+                    pattern = r'<link[\w\s\=\-\/\"\']*?href=[\'\"]{}\/{}[\'\"]\/>[\s]*'.format(dev_path[k], name)
+
+                file_php = re.sub(pattern, '', file_php)
+
+
+            if mode == 'dev':
+                #insert dev script/link tags
+                if k == 'js':
+                    pattern = r'<script src=\"{}\/{}\.min\.js\"><\/script>'.format(min_path[k], html_name.split('.')[-2])
+                    replace = "<script src=\"{}/{}\"></script>\n\t<script src=\"{}/{}.min.js\"></script>".format(dev_path[k], name, min_path[k], html_name.split('.')[-2])
+                else:
+                    pattern = r'<link rel=\'stylesheet\' href=\"{}\/{}\.min\.css\"\/>'.format(min_path[k], html_name.split('.')[-2])
+                    replace = "<link rel='stylesheet' href=\"{}/{}\"/>\n\t<link rel='stylesheet' href=\"{}/{}.min.css\"/>".format(dev_path[k], name, min_path[k], html_name.split('.')[-2])
+
+                file_php = re.sub(pattern, replace, file_php)
+
+
+
+        if mode == 'dev':
+            #erase prod script/link tag
+            if k == 'js':
+                pattern = r'<script src=\"{}\/{}\.min\.js\"><\/script>'.format(min_path[k], html_name.split('.')[-2])
+            else:
+                pattern = r'<link rel=\'stylesheet\' href=\"{}\/{}\.min\.css\"\/>'.format(min_path[k], html_name.split('.')[-2])
+
+            file_php = re.sub(pattern, '', file_php)
+
+            #normal permission for dev dir
+            os.system("chmod 755 {}/{}".format(html_path, dev_path[k]))
+
+
+    if mode == 'prod':
+        perc = float(files_done) / total_files * 20
+        print("[", end='')
+        for i in range(int(perc)):
+            print("=", end='')
+        print("{}".format(int(perc*5)%10), end='')
+        for i in range(19 - int(perc)):
+            print(" ", end='')
+        print("] {:.1f}%".format(perc*5))
+
+        if changes['js']:
+            data_path = "{}/{}/{}.min.js".format(html_path, min_path['js'], html_name.split('.')[-2])
+            print("Building {}...".format(data_path), end='')
+
+            # Define the parameters for the POST request and encode them in
+            # a URL-safe format.
+            p_array = [
+                ('compilation_level', 'SIMPLE_OPTIMIZATIONS'),
+                ('output_format', 'text'),
+                ('output_info', 'compiled_code'),
+            ]
+            for file in files['js']:
+                p_array.append(('js_code', "{}".format(file)))
+
+            params = urllib.urlencode(p_array)
+
+            # Always use the following value for the Content-type header.
+            headers = { "Content-type": "application/x-www-form-urlencoded" }
+            conn = httplib.HTTPSConnection('closure-compiler.appspot.com')
+            conn.request('POST', '/compile', params, headers)
+            response = conn.getresponse()
+            data = response.read()
+            
+            #write bundled minified file
+            f = open(data_path,"w")
+            f.write(data)
+            f.close()
+
+            print("DONE")
+
+            #print data
+            conn.close()
+        else:
+            print("No changes for {}/{}.min.js. Skipping.".format(min_path['js'], html_name.split('.')[-2]))
+
+        if changes['css']:
+            data_path = "{}/{}/{}.min.css".format(html_path, min_path['css'], html_name.split('.')[-2])
+            print("Building {}...".format(data_path), end='')
+
+            content = ""
+            for file in files['css']:
+                content = content + file
+
+            url = 'https://cssminifier.com/raw'
+            data = {'input': content}
+            out = requests.post(url, data=data).text
+
+            f = open(data_path,"w")
+            f.write(out)
+            f.close()
+            print("DONE")
+        else:
+            print("No changes for {}/{}.min.css. Skipping.".format(min_path['css'], html_name.split('.')[-2]))
+
+        files_done = files_done + 1
+
+    #write new php file
+    f = open("{}/{}".format(html_path, html_name), 'w')
+    f.write(file_php)
+    f.close()
+
+            
+
+print("Mode set to {}".format(mode))
+    
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<link rel="icon" type="image/gif" href="my_icon.png" />
+	<title>Index test page</title>
+	
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css"/>
+	
+	<link rel='stylesheet' href="css/index.min.css"/>
+	
+	<script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
+	<script src='https://code.jquery.com/ui/1.12.1/jquery-ui.min.js'></script>
+	
+	<script src="script/index.min.js"></script>
+</head>
+<body>
+	<h1>Test page</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</body>
+</html>
